b58b152079974465c52257efb899491efb136a0f
masaki <keitomasaki@icloud.com>
2021-09-02 20:27:48 +0900

    j

--- test/src/LoginButton.jsx
+++ test/src/LoginButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import FacebookLogin from "react-facebook-login";
+// import FacebookLogin from "react-facebook-login";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
 const LoginButton = () => {
   const responseFacebook = (response) => {
@@ -13,6 +14,8 @@ const LoginButton = () => {
       fields="name,email,picture"
       scope="instagram_basic,pages_show_list,public_profile"
       callback={responseFacebook}
+      icon=" fa-facebook "
+      render={(renderProps) => <button>This is my custom FB button</button>}
     />
   );
 };
