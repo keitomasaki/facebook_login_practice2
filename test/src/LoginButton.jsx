20f37cff692adf111818c9f6f39c13e5674c9ae4
masaki <keitomasaki@icloud.com>
2021-09-02 21:16:06 +0900

    hhd

--- test/src/LoginButton.jsx
+++ test/src/LoginButton.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-// import FacebookLogin from "react-facebook-login";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import FacebookLogin from "react-facebook-login";
+// import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
 const LoginButton = () => {
   const responseFacebook = (response) => {
-    console.log("response");
-    console.log(response);
+    try {
+      console.log("response2");
+      console.log(response);
+    } catch (error) {
+      console.error("error");
+      console.error(error);
+    }
   };
   return (
     <FacebookLogin
+      textButton="Facebookでログインする"
       appId="4499411150110925"
       autoLoad={true}
       fields="name,email,picture"
       scope="instagram_basic,pages_show_list,public_profile"
       callback={responseFacebook}
       icon=" fa-facebook "
-      render={(renderProps) => <button>This is my custom FB button</button>}
+      // render={(renderProps) => <button>This is my custom FB button</button>}
     />
   );
 };
